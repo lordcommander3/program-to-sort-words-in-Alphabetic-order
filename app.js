e880401a29637bb9d191a15538b610cf549b924e
zaryab <zaryabsheikh6@gmail.com>
2022-03-18 12:50:43 +0500

    Program to sort words in Alphabetic order

---
+++ app.js
@@ -0,0 +1,6 @@
+var string= prompt("Enter your text here");
+var stringArray = string.split(' ');
+var arranged= stringArray.sort();
+// document.write(string);
+// document.write(arranged);
+document.write(arranged.join(' '));
